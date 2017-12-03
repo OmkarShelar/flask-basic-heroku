419e3a5d5c06d984d56309c97e3125dba6789ea4
Omkar Shelar <omkarshelar24@gmail.com>
2017-12-03 09:35:59 +0530

    Added Heroku Postgres Connectivity

--- flask_demo.py
+++ flask_demo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from flask import Flask,request
-
+import dbconn
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
@@ -15,4 +15,13 @@ def n():
 	print(request.headers.get('User-Agent'))
 	print(request.remote_addr)
 	return "Bye!"
+
+@app.route('/<int:id>/', methods=['GET'])
+def dbr(id):
+	# print(request.headers.get('User-Agent'))
+	# print(request.remote_addr)
 	
+	return dbconn.dbretrieve(id)
+
+if __name__ == '__main__':
+	app.run()
